d78c2735524c01469cafdd45e80de005acb15785
blawney <blawney@hsph.harvard.edu>
2020-06-04 16:40:32 -0400

    Finalizing the dockerization.

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,9 +3,7 @@
 # This script manages the various services that need to be run on
 # startup of the container.
 
-# We need to setup the database
-
-# echo a pwd to the file
+# First we need to setup the database server and create a database to use
 echo $PGPASSWORD > /tmp/pwd.txt
 /usr/lib/postgresql/12/bin/initdb -D $HOME/postgres_data -A md5 --pwfile /tmp/pwd.txt $USER
 export PG_SOCKET_DIR=$HOME/postgres
@@ -15,28 +13,37 @@ mkdir $PG_SOCKET_DIR
 /usr/lib/postgresql/12/bin/psql -h localhost -p $DB_PORT -c "CREATE USER $DB_USER PASSWORD '$DB_PASSWD';" $DB_NAME
 /usr/lib/postgresql/12/bin/psql -h localhost -p $DB_PORT -c "ALTER USER $DB_USER CREATEDB;" $DB_NAME
 
-
 # Create directories that Django will use for uploaded files, etc.
 mkdir /www/pending_user_uploads
 mkdir /www/user_resources
 
-
+# Specify the appropriate settings file:
 if [ $ENVIRONMENT = 'dev' ]; then
     export DJANGO_SETTINGS_MODULE=mev.settings_dev
 else
     export DJANGO_SETTINGS_MODULE=mev.settings_production
 fi
 
+# Apply database migrations, collect the static files to server, and create
+# a superuser based on the environment variables passed to the container.
 python3 /www/manage.py makemigrations api
 python3 /www/manage.py migrate
 python3 /www/manage.py collectstatic --noinput
+python3 /www/manage.py createsuperuser --noinput
 
-# Startup redis, celery, gunicorn
+# Populate a "test" database, so the database
+# will have some content to query.
+if [ $POPULATE_DB = 'yes' ]; then
+    python3 /www/manage.py populate_db
+fi
+
+# Startup redis and celery
 supervisord -c /etc/supervisor/supervisord.conf
 supervisorctl reread && supervisorctl update
 
-cd /wwww
-gunicorn mev.wsgi:application $1
+# Start the application server:
+cd /www
+gunicorn mev.wsgi:application $@
 
 
 
