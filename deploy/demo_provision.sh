65f5aa23c1d12e74841eaf95c298eb7cc4b813d0
root <blawney@hsph.harvard.edu>
2021-05-03 11:10:37 -0400

    adding the demo provisioning script.

---
+++ deploy/demo_provision.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt-get update
+apt-get install -y apache2
+a2ensite default-ssl
+a2enmod ssl
+vm_hostname="$(curl -H "Metadata-Flavor:Google" http://169.254.169.254/computeMetadata/v1/instance/name)"
+echo "Page served from: $vm_hostname" | tee /var/www/html/index.html
+systemctl restart apache2
