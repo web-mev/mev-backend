9d84546dffcb3720c5e445ea19a92c7ae96385eb
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-05-04 18:48:34 -0400

    Set up Cromwell system user and log dir using Puppet

--- deploy/puppet/manifests/site.pp
+++ deploy/puppet/manifests/site.pp
@@ -4,6 +4,18 @@ node /cromwell/ {
   file { '/opt/cromwell-60.jar':
     source => 'https://github.com/broadinstitute/cromwell/releases/download/60/cromwell-60.jar'
   }
+
+  $cromwell_user = 'cromwell-runner'
+
+  user { $cromwell_user:
+    ensure => present,
+  }
+
+  file { '/var/log/cromwell':
+    ensure => directory,
+    owner  => $cromwell_user,
+    group  => $cromwell_user,
+  }
 }
 
 node /api/ {
