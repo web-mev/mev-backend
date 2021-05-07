02758bd6f4bfdc3e841831b228ae1c40626985bc
blawney <blawney@hsph.harvard.edu>
2021-05-07 08:12:33 -0400

    Adding a new dummy endpoint at root for easier health checks and fixing a unit test whose intentionally broken file ended up fixed somehow

--- mev/mev/urls.py
+++ mev/mev/urls.py
@@ -1,11 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def root_page(request):
+    return HttpResponse('The API is located at /api/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('/', include('api.urls')),
+    path('', root_page),
 ]
 
