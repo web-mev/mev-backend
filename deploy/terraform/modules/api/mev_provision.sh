1b5c18d9a07dea9b59fda0734b0c93364379c540
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-09-13 13:04:46 -0400

    Fix Cloud SQL Proxy error

--- deploy/terraform/modules/api/mev_provision.sh
+++ deploy/terraform/modules/api/mev_provision.sh
@@ -303,6 +303,7 @@ PATH="$PATH:/opt/puppetlabs/bin" && \
   /opt/puppetlabs/puppet/bin/librarian-puppet install
 
 /opt/puppetlabs/bin/puppet apply /opt/software/mev-backend/deploy/puppet/manifests/site.pp
+unset HOME  # for Cloud SQL Proxy
 
 usermod -aG docker $MEV_USER
 
