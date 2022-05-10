49d7095d6df9648d41e266a7076fe588edb73947
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-05-10 12:55:09 -0400

    wip: fixing syntax bug. still untested

--- mev/api/async_tasks/operation_tasks.py
+++ mev/api/async_tasks/operation_tasks.py
@@ -122,6 +122,7 @@ def check_executed_op(task_self, exec_op_uuid):
         # cromwell takes some time to parse inputs, etc.) the
         # call to check_status might return an error
         # do something here
+        pass
     if has_completed:
         logger.info('Job ({id}) has completed. Kickoff'
             ' finalization.'.format(
