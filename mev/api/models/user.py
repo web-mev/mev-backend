160751d09c67480afca6ad87044198d824f44e99
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-07-18 10:22:25 -0400

    Bumping django to 4.0.6 and updates to accommodate.

--- mev/api/models/user.py
+++ mev/api/models/user.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 '''
