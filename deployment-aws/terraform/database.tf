c023c2e7cbeb48bc6843eb3e8933857dc041ebd7
Brian Lawney <blawney@hsph.harvard.edu>
2022-08-10 10:57:08 -0400

    Reverting back to standard storage for DB since gp2 minimum size is excessive.

--- deployment-aws/terraform/database.tf
+++ deployment-aws/terraform/database.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "default" {
   engine_version             = "12"
   auto_minor_version_upgrade = true
   allocated_storage          = 5
-  storage_type               = "gp2"
+  storage_type               = "standard"
   username                   = "postgres"
   password                   = random_password.database_superuser.result
   final_snapshot_identifier  = "${local.common_tags.Name}-final-${random_id.database_snapshot.hex}"
