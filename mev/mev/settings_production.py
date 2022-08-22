d92232bddf09823aeb3a97d63bdfa638798ab16d
Ilya Sytchev <isytchev@hsph.harvard.edu>
2022-08-22 12:00:23 -0400

    Set email host and port correctly on AWS

--- mev/mev/settings_production.py
+++ mev/mev/settings_production.py
@@ -13,6 +13,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 ###############################################################################
