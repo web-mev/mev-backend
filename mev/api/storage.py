923e212f681f95a619e25e21f0206f7a0ffe427d
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-08-22 21:08:07 -0400

    Fix typo/bug in name of custom S3 storage class

--- mev/api/storage.py
+++ mev/api/storage.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-class S3MediaStorage(S3Boto3Storage):
+class S3ResourceStorage(S3Boto3Storage):
     bucket_name = settings.MEDIA_ROOT
