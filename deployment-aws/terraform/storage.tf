d8b8b50d55d4f9357d3aeefee47df929961025b4
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-09-07 15:34:48 -0400

    Adding CORS policy to main storage bucket

--- deployment-aws/terraform/storage.tf
+++ deployment-aws/terraform/storage.tf
@@ -5,3 +5,15 @@ resource "aws_s3_bucket" "api_storage_bucket" {
 resource "aws_s3_bucket" "cromwell_storage_bucket" {
     bucket = "${local.stack}-cromwell-storage"
 }
+
+resource "aws_s3_bucket_cors_configuration" "storage_bucket_cors" {
+  bucket = aws_s3_bucket.api_storage_bucket.id
+
+  cors_rule {
+    allowed_headers = ["RANGE", "Cache-control", "If-None-Match", "Content-Type"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["https://${var.frontend_domain}"]
+    expose_headers  = ["Content-Length", "Content-Range", "Content-Type"]
+    max_age_seconds = 3000
+  }
+}
