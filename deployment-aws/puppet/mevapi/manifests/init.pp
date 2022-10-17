3e619522b34fe73ed671068663f1822d68e5b357
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-10-17 19:55:17 -0400

    Adding back puppetlabs-lvm for creating filesystem

--- deployment-aws/puppet/mevapi/manifests/init.pp
+++ deployment-aws/puppet/mevapi/manifests/init.pp
@@ -80,6 +80,13 @@ class mevapi (
 
   if $platform == 'aws' {
 
+    # https://forge.puppetlabs.com/puppetlabs/lvm
+    filesystem { $data_volume_device_name:
+      ensure  => present,
+      fs_type => 'ext4',
+      before  => File[$data_root],
+    }
+
     mount { $data_root:
       ensure  => mounted,
       device  => $data_volume_device_name,
