5eb92820dfe6ce9ec481f8e5271c599e500d2236
Ilya Sytchev <isytchev@hsph.harvard.edu>
2022-10-26 14:27:34 -0400

    Lower healthy check threshold

--- deployment-aws/terraform/loadbalancer.tf
+++ deployment-aws/terraform/loadbalancer.tf
@@ -16,6 +16,9 @@ resource "aws_lb_target_group" "api" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  health_check {
+    healthy_threshold   = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "api" {
