2befa2887a8c5f4d4ddca6528f3a736f0bf19b77
Ilya Sytchev <isytchev@hsph.harvard.edu>
2022-11-18 12:27:52 -0500

    Prevent agent config from running more than once

--- deployment-aws/puppet/cromwell/manifests/config.pp
+++ deployment-aws/puppet/cromwell/manifests/config.pp
@@ -34,5 +34,6 @@ class cromwell::config () {
   exec { 'cloudwatch_agent':
     command => "${cromwell::cloudwatch_agent_dir}/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 \
     -c file:${cromwell::cloudwatch_agent_dir}/etc/amazon-cloudwatch-agent.json",
+    creates => "${cromwell::cloudwatch_agent_dir}/etc/amazon-cloudwatch-agent.d/file_amazon-cloudwatch-agent.json"
   }
 }
