4a8b43f29297657f47a761809b0a6c5a7254d173
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-12-15 13:24:24 -0500

    Removing tf/puppet code for deployment on gcp. No longer needed

--- deploy/terraform/modules/cromwell/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cromwell_ip" {
-    value = google_compute_instance.cromwell.network_interface.0.network_ip
-}
-
-output "cromwell_bucket" {
-    value = google_storage_bucket.cromwell_bucket.url
-}
