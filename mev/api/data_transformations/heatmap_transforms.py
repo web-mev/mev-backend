a7a11a341ec7e2de92bc15537f2e2291bf527871
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-03-20 20:47:36 -0400

    adding clustering method for a feature-set derived heatmap

--- mev/api/data_transformations/heatmap_transforms.py
+++ mev/api/data_transformations/heatmap_transforms.py
@@ -11,9 +11,51 @@ from constants import MATRIX_KEY, \
     FEATURE_TABLE_KEY
 from data_structures.attribute_types import PositiveIntegerAttribute
 
-from resource_types import get_resource_type_instance
+from resource_types import get_resource_type_instance, get_contents
 
 logger = logging.getLogger(__name__)
+
+
+def perform_clustering(df, method, metric, resource_type_instance):
+    '''
+    See scipy reference for args. For metric, we have:
+    ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’
+    For method, we have:
+    'single','complete', 'average', 'centroid','median', 'ward', 'weighted'
+    
+    Note that some methods require particular metrics (e.g. ward requires euclidean).
+    Rather than worry about that, we just generically catch exceptions raised by
+    the linkage method
+
+    Recall that scipy operates transposed to how we consider matrices. That is, 
+    in the scipy world, ROWS of a matrix are observations. Our convention (based
+    on expression matrices) is to have observations in columns.
+    '''
+    try:
+        row_linkage = linkage(df, method=method, metric=metric)
+        col_linkage = linkage(df.T, method=method, metric=metric)
+    except ValueError as ex:
+        logger.info('Failed to create linkage. Reason was: {x}'.format(x=ex))
+        raise Exception(str(ex))
+
+    row_dendrogram = dendrogram(
+        row_linkage,
+        no_plot=True
+    )
+    col_dendrogram = dendrogram(
+        col_linkage,
+        no_plot=True
+    )
+
+    # the `leaves` key has the ordering as read from
+    # left-to-right (or top to bottom).
+    row_order = df.index[row_dendrogram['leaves']]    
+    col_order = df.columns[col_dendrogram['leaves']]
+
+    # reorder the matrix to correspond to the clustering
+    df = df.loc[row_order, col_order]
+    # convert to our usual return payload
+    return resource_type_instance.to_json(df)
 
 
 def heatmap_reduce(resource, query_params):
@@ -70,42 +112,37 @@ def heatmap_reduce(resource, query_params):
     # use that sort order to subset the matrix:
     df = df.iloc[sort_ordering]
 
-    '''
-    See scipy for values. For metric, we have:
-    ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’
-    For method, we have:
-    'single','complete', 'average', 'centroid','median', 'ward', 'weighted'
-    
-    Note that some methods require particular metrics (e.g. ward requires euclidean).
-    Rather than worry about that, we just generically catch exceptions raised by
-    the linkage method
+    return perform_clustering(df, method, metric, resource_type_instance)
 
-    Recall that scipy operates transposed to how we consider matrices. That is, 
-    in the scipy world, ROWS of a matrix are observations. Our convention (based
-    on expression matrices) is to have observations in columns.
-    '''
+
+def heatmap_cluster(resource, query_params):
+
+    # note that we pop the transform-specific keys so we can submit
+    # the remaining filters to the "general" dataframe filtering
     try:
-        row_linkage = linkage(df, method=method, metric=metric)
-        col_linkage = linkage(df.T, method=method, metric=metric)
-    except ValueError as ex:
-        logger.info('Failed to create linkage. Reason was: {x}'.format(x=ex))
-        raise Exception(str(ex))
+        metric = query_params.pop('metric')
+    except KeyError:
+        metric = 'euclidean'
 
-    row_dendrogram = dendrogram(
-        row_linkage,
-        no_plot=True
-    )
-    col_dendrogram = dendrogram(
-        col_linkage,
-        no_plot=True
-    )
+    try:
+        method = query_params.pop('method')
+    except KeyError:
+        method = 'ward'
 
-    # the `leaves` key has the ordering as read from
-    # left-to-right (or top to bottom).
-    row_order = df.index[row_dendrogram['leaves']]    
-    col_order = df.columns[col_dendrogram['leaves']]
+    acceptable_resource_types = [
+        MATRIX_KEY,
+        EXPRESSION_MATRIX_KEY,
+        INTEGER_MATRIX_KEY,
+        RNASEQ_COUNT_MATRIX_KEY,
+        FEATURE_TABLE_KEY
+    ]
+    if not resource.resource_type in acceptable_resource_types:
+        raise Exception('Not an acceptable resource type for this function.')
 
-    # reorder the matrix to correspond to the clustering
-    df = df.loc[row_order, col_order]
-    # convert to our usual return payload
-    return resource_type_instance.to_json(df)
+    resource_type_instance = get_resource_type_instance(resource.resource_type)
+    # Note that the following method returns a json-format data structure
+    # since it's typically used by a view. However, we use it to populate
+    # an attribute on the instance which has the underlying dataframe
+    get_resource_type_instance.get_contents(resource, query_params)
+    df = resource_type_instance.table
+    return perform_clustering(df, method, metric, resource_type_instance)
